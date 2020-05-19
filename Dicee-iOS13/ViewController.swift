04cdbdff3b42f2dac7b0654c58bab64f1e030e49
Pavel Barysevich <pavelbarysevich@gmail.com>
2020-05-19 15:11:27 +0300

    1. Dicee

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Интерфейс с кодом дружим
+    @IBOutlet var diceIamgeView1: UIImageView!
+    @IBOutlet var diceImageView2: UIImageView!
+    
+  
+    // Действие внешнего вида объекта
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
+    
+    // Акшен кнопки
+    @IBAction func rollButtomPresset(_ sender: UIButton) {
+        
+        // создаем константу - массив коcтей из 6 объектов - ссылаясь на его
+        let daceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        
+        // ссылаясь на  Аутлеты картинки, ссылка на Массив / Рандом костей
+        diceIamgeView1.image = daceArray.randomElement()
+        diceImageView2.image = daceArray[Int.random(in: 0...5)]
+        
+        
+    }
+    
 }
 
